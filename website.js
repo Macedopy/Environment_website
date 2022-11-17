aa9146a34dd7f1a0518527336244f40317433658
Macedo <108029211+Macedopy@users.noreply.github.com>
2022-11-17 17:39:36 -0300

    Add files via upload

---
+++ website.js
@@ -0,0 +1,23 @@
+function showMenu(){
+    document.querySelector(".navegation-link-div").style.right = '0px';
+}
+
+function hideMenu(){
+    document.querySelector(".navegation-link-div").style.right = '-300px';
+}
+
+function Scrolltologin() {
+  document.getElementsByClassName('main-login')[3].scrollIntoView();
+}
+
+function Scrolltoinicio() {
+  document.getElementsByClassName('inicio')[3].scrollIntoView();
+}
+
+function Scrolltosobre() {
+  document.getElementsByClassName('sobre')[3].scrollIntoView();
+}
+
+function Scrolltocontato() {
+  document.getElementsByClassName('contato')[3].scrollIntoView();
+}
